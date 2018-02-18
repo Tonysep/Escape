60598ed667894199b5a2f6c15b1e914c2e389a1b
Tony <tonysaltodev@gmail.com>
2018-02-18 20:00:37 -0300

    Upload UELOG test

--- BuldingEscape/BuldingEscapeGame/Source/BuldingEscapeGame/PositionReport.cpp
+++ BuldingEscape/BuldingEscapeGame/Source/BuldingEscapeGame/PositionReport.cpp
@@ -9,6 +9,9 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+    
+    UE_LOG(LogTemp, Warning, Text("Testando UE4"));
+    
 
 	// ...
 }
